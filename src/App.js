a98837265b7458166d78a76992e16547295a4180
Mylo James <mjames@appacademy.io>
2021-01-17 15:23:50 -0600

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import AllAnimals from './components/AllAnimals';
 import { Switch, Route } from 'react-router-dom';
+import AllAnimals from './components/AllAnimals';
+import AddAnimal from './components/AddAnimal';
+
 function App() {
     return (
         <>
             <Switch>
                 <Route exact path='/'>
                     <AllAnimals />
+                </Route>
+                <Route exact path='/add'>
+                    <AddAnimal />
                 </Route>
             </Switch>
         </>
