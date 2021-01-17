7eb6172850823ba417bb1bfaba353627c41d13d6
Mylo James <mjames@appacademy.io>
2021-01-17 14:48:29 -0600

    start of context

---
+++ src/context/AnimalContext.js
@@ -0,0 +1,19 @@
+/* 
+    Create a context - create a file cab
+    Create provider component - give component access to the file cab
+    Provide a value to the context - fill the file cab
+    Create custom hook - grab info from file cab 
+*/
+
+import { createContext } from 'react';
+
+const animalContext = createContext();
+
+const AnimalContextProvider = () => { 
+{/* 
+    animalContext.provider
+    give access to it's children
+*/}
+}
+
+export default AnimalContextProvider
