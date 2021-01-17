70268e8d4b19980d709ea31a2addb68320dc79bc
Mylo James <mjames@appacademy.io>
2021-01-17 14:34:06 -0600

    finished map

--- src/components/AllAnimals/AllAnimals.js
+++ src/components/AllAnimals/AllAnimals.js
@@ -3,14 +3,26 @@ const AllAnimals = () => {
     console.log(animals);
     return (
         <>
-            <h1>AllAnimals</h1>
+            <h1>All Animals</h1>
             {/* 
                 display for each animal
                   h3 of name
                   p of type
                   p of bio
                   if asleep p of asleep else p of awake
-            */ }
+            */}
+            <ul>
+                {animals.map((animal) => {
+                    return (
+                        <li key={`${animal.name}-${animal.type}`}>
+                            <h3>{animal.name}</h3>
+                            <p>{animal.type}</p>
+                            <p>{animal.bio}</p>
+                            <>{animal.asleep ? 'asleep' : 'awake'}</>
+                        </li>
+                    );
+                })}
+            </ul>
         </>
     );
 };
