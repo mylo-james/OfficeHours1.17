f3fe71b205a073f7e509c4913540ab6a8ff66da7
Mylo James <mjames@appacademy.io>
2021-01-17 13:55:24 -0600

    added some data and app thoughts

---
+++ src/data/animals.js
@@ -0,0 +1,40 @@
+export const animals = [
+    {
+        name: 'Ellie',
+        type: 'elephant',
+        bio: 'Ellie is an elephant who loves to jump',
+        asleep: true,
+    },
+    {
+        name: 'Jeff',
+        type: 'crocodile',
+        bio: 'Jeff is a crocodile who loves to swim',
+        asleep: false,
+    },
+    {
+        name: 'Sam',
+        type: 'elephant',
+        bio: 'Sam is an elephant who loves to eat',
+        asleep: true,
+    },
+    {
+        name: 'Jerry',
+        type: 'mouse',
+        bio: 'Jerry is a mouse who loves to scare elephants',
+        asleep: false,
+    },
+    {
+        name: 'Tom',
+        type: 'cat',
+        bio: 'Tom is a cat who loves to chase mice',
+        asleep: true,
+    },
+    {
+        name: 'Stewart',
+        type: 'mouse',
+        bio: 'Stewart is a mouse who loves to act like a human',
+        asleep: true,
+    },
+];
+
+export const animalsTypes = ['cat', 'mouse', 'elephant', 'crocodile'];
